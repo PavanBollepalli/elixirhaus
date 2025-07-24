1142863e190b6dbc06a418b825d100788d160448
pavan <pavanvenkatanagamanoj@gmail.com>
2025-07-24 15:10:07 +0530

    nav bar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section id="hero" className='noisy'>
+    <h1 className='title'>MOJITO</h1>
+    <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
+    <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
+    <div className='body'>
+        <div className="content">
+            <div className='space-y-5 hidden md:block'>
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Hero
